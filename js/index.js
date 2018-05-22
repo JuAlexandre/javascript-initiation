025b4204553191713cc544a8de4c0abb52e8fbfe
Julien <julien.alexandre26@gmail.com>
2018-05-22 16:59:33 +0200

    Solution de la quête Javascripting.

---
+++ js/index.js
@@ -0,0 +1,20 @@
+console.log('Ca fonctionne !');
+
+let choice = prompt('Hey mon ami ! Tu aimes ça les patates ?');
+
+if (choice.toLowerCase() == 'oui') {
+    
+  for (let i = 1; i < 8; i++) {
+    console.log('#'.repeat(i));
+  }
+  
+  
+} else if (choice.toLowerCase() == 'non') {
+  
+  alert('C\'est dommage c\'est bon les patates !');
+  
+} else {
+  
+  alert('Je vous sens comme tiraillé ...');
+  
+}
